885d4a45af83e9d7ce9739db27bf2f6d9ee217a9
aroarian <aroarian0@gmail.com>
2018-06-26 10:48:07 -0400

    underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,25 @@
-var words = ["pickle","motherboard","absurd","exodus","ivy","quartz","unworthy","witchcraft","transcript","kilobyte"];
+var words = [
+  "pickle",
+  "motherboard",
+  "absurd",
+  "exodus",
+  "ivy",
+  "quartz",
+  "unworthy",
+  "witchcraft",
+  "transcript",
+  "kilobyte"
+];
 var currentWord = [];
 
-window.onload = function(event){
-    var wordPick = words[Math.floor(Math.random() * words.length)];
-    currentWord.push(wordPick);
-    console.log(currentWord);
+window.onload = function(event) {
+  var wordPick = words[Math.floor(Math.random() * words.length)];
+  currentWord.push(wordPick);
+  var wordString = currentWord.toString();
+  var wordLength = wordString.length;
+  var underScore = "‾ ".repeat(wordLength);
+  document.getElementById("underscore").innerHTML= underScore;
+  console.log(wordLength);
+  console.log(currentWord);
+  console.log(wordString);
 };
