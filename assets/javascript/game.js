731229684ebf2f91e052fbdf9f378b126b71fd50
aroarian <aroarian0@gmail.com>
2018-06-28 21:25:11 -0400

    images logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,13 +25,8 @@ function gamereset() {
   underScores = [];
   document.getElementById("underscore").innerHTML = "";
   document.getElementById("userguess").innerHTML = "";
-  // document.getElementById("loss").innerHTML = guessLeft;
-  // document.getElementById("wins").innerHTML = win;
-  // var wordPick = words[Math.floor(Math.random() * words.length)];
-  // var currentWord = wordPick;
-  // var split = currentWord.split("");
   gamestart();
-  console.log(split);
+  
 
   for (var i = 0; i < wordPick.length; i++) {
     underScores.push("_ ");
@@ -41,8 +36,11 @@ function gamereset() {
 function gamestart() {
   underScores = [];
   guessLeft = 10;
-  // var imgstart = document.createElement("img");
-  // imgstart.setAttribute("src", "images/hydrangeas.jpg");
+  var img = document.createElement("img");
+  img.setAttribute("src", "assets/images/start.jpg");
+  img.setAttribute("height", "434");
+  img.setAttribute("width", "386");
+  document.getElementById("images").appendChild(img);
   document.getElementById("userguess").innerHTML = "";
   document.getElementById("loss").innerHTML = guessLeft;
   document.getElementById("wins").innerHTML = win;
@@ -89,7 +87,6 @@ function gamestart() {
       alert("Congrats you win! The word was: " + wordPick);
       win++;
       document.getElementById("wins").innerHTML = win;
-      
       gamestart();
     } 
 
@@ -97,7 +94,36 @@ function gamestart() {
       alert("You Lose. The word was: " + wordPick);
       win = 0;
       guessLeft = 10;
+      document.getElementById("images").innerHTML = "";
       gamereset();
+    }
+
+    if (guessLeft == 9){
+      img.setAttribute("src", "assets/images/9.jpg");
+    }
+    if (guessLeft == 8){
+      img.setAttribute("src", "assets/images/8.jpg");
+    }
+    if (guessLeft == 7){
+      img.setAttribute("src", "assets/images/7.jpg");
+    }
+    if (guessLeft == 6){
+      img.setAttribute("src", "assets/images/6.jpg");
+    }
+    if (guessLeft == 5){
+      img.setAttribute("src", "assets/images/5.jpg");
+    }
+    if (guessLeft == 4){
+      img.setAttribute("src", "assets/images/4.jpg");
+    }
+    if (guessLeft == 3){
+      img.setAttribute("src", "assets/images/3.jpg");
+    }
+    if (guessLeft == 2){
+      img.setAttribute("src", "assets/images/2.jpg");
+    }
+    if (guessLeft == 1){
+      img.setAttribute("src", "assets/images/1.jpg");
     }
   }
 };
